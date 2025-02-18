5a922125161338bfbf613c4c6a2240f963254652
shaheer-haider <shaheerhaider00000@gmail.com>
2025-02-18 13:42:07 +0500

    fixed threadpool execution for parallel scrapping

--- lambda_function.py
+++ lambda_function.py
@@ -13,48 +13,18 @@ dotenv.load_dotenv()
 aws_key = os.environ.get('AWS_ACCESS_KEY_ID')
 aws_secret = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-def trigger_brand_scraping(brand_name, brand_id, dt):
-    if __name__ == "__main__":
-        print(f"Starting scraping process for brand: {brand_name}")
-        with ThreadPoolExecutor() as executor:
-            executor.submit(scrap_single_brand, brand_name, brand_id, dt, len(BRAND_IDS.keys()))
-    else:
-        lambda_client = boto3.client('lambda')
-        lambda_client.invoke(
-            FunctionName='salesdeep-scrapping',
-            InvocationType='Event',
-            Payload=json.dumps({
-                "action": "scrap_brand",
-                "brand_name": brand_name,
-                "brand_id": brand_id,
-                "dt": dt,
-                "number_of_brands": len(BRAND_IDS.keys())
-            })
-        )
-
-def distribute_scraping(event, context):
+def distribute_scraping():
     dt = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    for brand_name, brand_id in BRAND_IDS.items():
-        trigger_brand_scraping(brand_name, brand_id, dt)
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(lambda brand: scrap_single_brand(brand[0], brand[1], dt, len(BRAND_IDS.keys())), BRAND_IDS.items())
+
     return {
         'statusCode': 200,
         'body': f'Started scraping {len(BRAND_IDS)} brands'
     }
 
-def lambda_handler(event, context):
-    actions = {
-        "default": distribute_scraping,
-        "scrap_brand": lambda e, c: scrap_single_brand(
-            e.get('brand_name'),
-            e.get('brand_id'),
-            e.get('dt'),
-            e.get('number_of_brands')
-        )
-    }
-
-    action = event.get("action", "default")
-    return actions[action](event, context)
-
 if __name__ == "__main__":
-    lambda_handler({}, None)
+    distribute_scraping()
+
 print(__name__)
